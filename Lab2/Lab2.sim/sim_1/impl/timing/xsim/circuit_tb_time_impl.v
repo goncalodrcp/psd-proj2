6117974545e3a4567301491301fc1e4d9f10fa7e
José Coelho <josecoelho96@gmail.com>
2018-11-20 01:01:02 +0000

    Update final version

--- Lab2/Lab2.sim/sim_1/impl/timing/xsim/circuit_tb_time_impl.v
+++ Lab2/Lab2.sim/sim_1/impl/timing/xsim/circuit_tb_time_impl.v
@@ -1,7 +1,7 @@
 // Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2018.2 (lin64) Build 2258646 Thu Jun 14 20:02:38 MDT 2018
-// Date        : Mon Nov 19 00:35:14 2018
+// Date        : Mon Nov 19 15:54:54 2018
 // Host        : coelhopc running 64-bit Ubuntu 18.10
 // Command     : write_verilog -mode timesim -nolib -sdf_anno true -force -file
 //               /home/josecoelho/Documents/IST/PSD/Labs/Lab2/psd-proj2/Lab2/Lab2.sim/sim_1/impl/timing/xsim/circuit_tb_time_impl.v
